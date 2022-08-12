7b78f18b27edf1846bb69a11b0f83cd6fec38149
Shetriga <mahmoud.atef98@outlook.com>
2022-08-12 17:28:39 +0200

    First Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { SuccessfulLoginComponent } from './successful-login/successful-login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full'},
+  { path: 'login', component: LoginComponent},
+  { path: 'success', component: SuccessfulLoginComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
